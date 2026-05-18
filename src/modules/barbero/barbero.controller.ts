ac3365c6e99288fd6de1c3416f4d831614a23550
Carlos Abel <135867557+Carlos-Abel-17@users.noreply.github.com>
2026-05-18 14:23:11 -0500

    poniendo en publico el create barbero

--- src/modules/barbero/barbero.controller.ts
+++ src/modules/barbero/barbero.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { BarberoService } from './barbero.service';
 import { CreateBarberoDto } from './dto/barbero.dto';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('barbero')
 export class BarberoController {
   constructor(private readonly barberoSrv: BarberoService) {}
 
+  @Public()
   @Post('create')
   createBarbero(@Body() barbero: CreateBarberoDto) {
     return this.barberoSrv.createBarbero(barbero);
