0435a985416b4824dce916e76e48c17e49e58bfb
Carlos Abel <aguado170305@gmail.com>
2026-06-03 11:50:36 -0500

    [FIX] solucion de error de importacion en negocio

--- src/modules/negocio/negocio.module.ts
+++ src/modules/negocio/negocio.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { NegocioController } from './negocio.controller';
 import { NegocioService } from './negocio.service';
 import { TipoNegocioController } from './tipo-negocio/tipo-negocio.controller';
 import { TipoNegocioService } from './tipo-negocio/tipo-negocio.service';
+import { NegocioEntity } from './entity/negocio.entity';
+import { TipoNegocioEntity } from './entity/tipo-negocio.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([NegocioEntity, TipoNegocioEntity])],
   controllers: [NegocioController, TipoNegocioController],
   providers: [NegocioService, TipoNegocioService],
-  exports: [NegocioService, TipoNegocioService],
+  exports: [NegocioService, TipoNegocioService, TypeOrmModule],
 })
 export class NegocioModule {}
