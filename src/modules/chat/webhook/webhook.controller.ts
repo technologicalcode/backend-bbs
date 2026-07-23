c6f15ec02300a174e6ed1fb42ffa8d9f70c7f77c
Carlos Abel <135867557+Carlos-Abel-17@users.noreply.github.com>
2026-07-22 23:47:30 -0500

    webhook verificacion wsp

--- src/modules/chat/webhook/webhook.controller.ts
+++ src/modules/chat/webhook/webhook.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, Query, Res } from "@nestjs/common";
 import { WhatsAppService } from "../whatsApp/whatsApp.service";
 import { Public } from "src/auth/decorators/public.decorator";
+import { Response } from "express";
 
 @Controller('webhook')
 @Public()
@@ -12,6 +13,23 @@ export class WebhookController {
   @Post('whatsApp')
   async whatsAppWebhook(@Body() body: any) {
     return this.whatsAppService.ServerWebhookWhatsApp(body);
+  }
+  
+  @Get('whatsApp')
+  async VerifyTokenWhatsApp(
+    @Query('hub.mode') mode: string,
+    @Query('hub.verify_token') token: string,
+    @Query('hub.challenge') challenge: string,
+    @Res() res: any,
+  ) {
+    if (
+      mode === 'subscribe' &&
+      token === process.env.WEBHOOK_VERIFY_TOKEN
+    ) {
+      return res.status(200).send(challenge);
+    }
+  
+    return res.sendStatus(403);
   }
   
 }
